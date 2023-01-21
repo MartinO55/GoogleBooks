a94f751d17cfe4c333ca4af7f652014a8316aff2
Martin Olson <martinguyolson@gmail.com>
2023-01-21 12:38:21 +1100

    added todos

--- GoogleBooks/src/App.jsx
+++ GoogleBooks/src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* TODO: return header */}
+
+      {/* TODO: Return Search bar */}
+
+      {/* TODO: Return Cards of Books */}
+    </>
+  );
 }
 
 export default App;
